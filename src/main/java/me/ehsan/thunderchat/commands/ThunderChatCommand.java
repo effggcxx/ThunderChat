6ab156ee30250fd00b49fac674d2891758575a87
Ehsan <mhmdahsanhadhqypwr@gmail.com>
2026-08-16 11:21:38 +0330

    Secure and expand the ThunderChat admin command

--- src/main/java/me/ehsan/thunderchat/commands/ThunderChatCommand.java
+++ src/main/java/me/ehsan/thunderchat/commands/ThunderChatCommand.java
@@ -11,12 +11,24 @@ public class ThunderChatCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            plugin.loadPluginConfig();
-            sender.sendMessage("§6[ThunderChat] §aConfig reloaded.");
+        if (!sender.hasPermission("thunderchat.admin")) {
+            sender.sendMessage("§cYou don't have permission to use this command.");
             return true;
         }
-        // TODO: other admin subcommands
+        if (args.length == 0 || args[0].equalsIgnoreCase("info")) {
+            sender.sendMessage("§6§lThunderChat");
+            sender.sendMessage("§7Version: §f" + plugin.getPluginMeta().getVersion());
+            sender.sendMessage("§7Channels: §f6 §7| Filters: §f" + (plugin.getFilterManager().isEnabled() ? "enabled" : "disabled"));
+            sender.sendMessage("§7Use §f/thunderchat reload §7to reload configuration.");
+            return true;
+        }
+        if (args[0].equalsIgnoreCase("reload")) {
+            plugin.loadPluginConfig();
+            plugin.getMuteManager().reload();
+            sender.sendMessage("§6[ThunderChat] §aConfiguration and persistent mute data reloaded.");
+            return true;
+        }
+        sender.sendMessage("§cUsage: /thunderchat <reload|info>");
         return true;
     }
 }
