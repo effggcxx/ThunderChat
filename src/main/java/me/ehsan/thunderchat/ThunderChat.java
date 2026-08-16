61f3512d972a316bded18abf51cb7c24944fdb96
Ehsan <mhmdahsanhadhqypwr@gmail.com>
2026-08-16 11:22:47 +0330

    Register mute list and player state cleanup listener

--- src/main/java/me/ehsan/thunderchat/ThunderChat.java
+++ src/main/java/me/ehsan/thunderchat/ThunderChat.java
@@ -4,6 +4,7 @@ import me.ehsan.thunderchat.alerts.AlertManager;
 import me.ehsan.thunderchat.channels.GlobalChatManager;
 import me.ehsan.thunderchat.channels.GlobalChatManager.Channel;
 import me.ehsan.thunderchat.commands.ChannelCommand;
+import me.ehsan.thunderchat.commands.ChannelMuteListCommand;
 import me.ehsan.thunderchat.commands.ChatChannelCommand;
 import me.ehsan.thunderchat.commands.ChatHideCommand;
 import me.ehsan.thunderchat.commands.ClearChatCommand;
@@ -15,6 +16,7 @@ import me.ehsan.thunderchat.commands.ThunderChatCommand;
 import me.ehsan.thunderchat.filter.CapsManager;
 import me.ehsan.thunderchat.filter.FilterManager;
 import me.ehsan.thunderchat.listeners.ChatListener;
+import me.ehsan.thunderchat.listeners.PlayerStateListener;
 import me.ehsan.thunderchat.messaging.IgnoreManager;
 import me.ehsan.thunderchat.messaging.PrivateMessageManager;
 import me.ehsan.thunderchat.muting.MuteManager;
@@ -38,24 +40,58 @@ public final class ThunderChat extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        instance = this; loadPluginConfig();
-        this.muteManager = new MuteManager(this); this.globalChatManager = new GlobalChatManager(this); this.alertManager = new AlertManager(this);
-        this.filterManager = new FilterManager(this); this.capsManager = new CapsManager(this); this.messageManager = new PrivateMessageManager(this);
-        this.ignoreManager = new IgnoreManager(this); this.spyManager = new SpyManager(this);
+        instance = this;
+        loadPluginConfig();
+        this.muteManager = new MuteManager(this);
+        this.globalChatManager = new GlobalChatManager(this);
+        this.alertManager = new AlertManager(this);
+        this.filterManager = new FilterManager(this);
+        this.capsManager = new CapsManager(this);
+        this.messageManager = new PrivateMessageManager(this);
+        this.ignoreManager = new IgnoreManager(this);
+        this.spyManager = new SpyManager(this);
+
         getServer().getPluginManager().registerEvents(new ChatListener(this), this);
         getServer().getPluginManager().registerEvents(new SpyListener(this), this);
-        getCommand("msg").setExecutor(new MsgCommand(this)); getCommand("reply").setExecutor(new ReplyCommand(this));
+        getServer().getPluginManager().registerEvents(new PlayerStateListener(this), this);
+
+        getCommand("msg").setExecutor(new MsgCommand(this));
+        getCommand("reply").setExecutor(new ReplyCommand(this));
         IgnoreCommand ignoreCmd = new IgnoreCommand(this); getCommand("ignore").setExecutor(ignoreCmd); getCommand("unignore").setExecutor(ignoreCmd);
-        getCommand("channel").setExecutor(new ChannelCommand(this)); getCommand("clearchat").setExecutor(new ClearChatCommand(this));
-        getCommand("thunderchat").setExecutor(new ThunderChatCommand(this)); getCommand("chat").setExecutor(new ChatChannelCommand(this, null));
-        getCommand("chathide").setExecutor(new ChatHideCommand(this)); getCommand("staffchat").setExecutor(new ChatChannelCommand(this, Channel.STAFF));
-        getCommand("donatorchat").setExecutor(new ChatChannelCommand(this, Channel.DONATOR)); getCommand("adminchat").setExecutor(new ChatChannelCommand(this, Channel.ADMIN));
-        getCommand("highrankchat").setExecutor(new ChatChannelCommand(this, Channel.HIGHRANK)); getCommand("gc").setExecutor(new ChatChannelCommand(this, Channel.GLOBAL));
-        getCommand("spy").setExecutor(new SpyCommand(this)); printEnableBanner();
+        getCommand("channel").setExecutor(new ChannelCommand(this));
+        getCommand("channelmutelist").setExecutor(new ChannelMuteListCommand(this));
+        getCommand("clearchat").setExecutor(new ClearChatCommand(this));
+        getCommand("thunderchat").setExecutor(new ThunderChatCommand(this));
+        getCommand("chat").setExecutor(new ChatChannelCommand(this, null));
+        getCommand("chathide").setExecutor(new ChatHideCommand(this));
+        getCommand("staffchat").setExecutor(new ChatChannelCommand(this, Channel.STAFF));
+        getCommand("donatorchat").setExecutor(new ChatChannelCommand(this, Channel.DONATOR));
+        getCommand("adminchat").setExecutor(new ChatChannelCommand(this, Channel.ADMIN));
+        getCommand("highrankchat").setExecutor(new ChatChannelCommand(this, Channel.HIGHRANK));
+        getCommand("gc").setExecutor(new ChatChannelCommand(this, Channel.GLOBAL));
+        getCommand("spy").setExecutor(new SpyCommand(this));
+        printEnableBanner();
     }
-    @Override public void onDisable() { if (ignoreManager != null) ignoreManager.save(); if (spyManager != null) spyManager.save(); getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[ThunderChat] " + ChatColor.RED + "Disabled."); }
+
+    @Override
+    public void onDisable() {
+        if (muteManager != null) muteManager.save();
+        if (ignoreManager != null) ignoreManager.save();
+        if (spyManager != null) spyManager.save();
+        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[ThunderChat] " + ChatColor.RED + "Disabled.");
+    }
+
     public void loadPluginConfig() { saveDefaultConfig(); reloadConfig(); this.config = getConfig(); }
-    private void printEnableBanner() { String prefix = ChatColor.GOLD + "" + ChatColor.BOLD + "ThunderChat" + ChatColor.RESET; String version = getPluginMeta().getVersion(); getServer().getConsoleSender().sendMessage(""); getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "  ⚡ " + prefix + ChatColor.GRAY + " v" + version); getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  ✔ Plugin enabled"); getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  ✔ Config loaded " + ChatColor.GRAY + "(" + config.getKeys(true).size() + " keys)"); getServer().getConsoleSender().sendMessage(""); }
+    private void printEnableBanner() {
+        String prefix = ChatColor.GOLD + "" + ChatColor.BOLD + "ThunderChat" + ChatColor.RESET;
+        String version = getPluginMeta().getVersion();
+        getServer().getConsoleSender().sendMessage("");
+        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "  ⚡ " + prefix + ChatColor.GRAY + " v" + version);
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  ✔ Plugin enabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "  ✔ Config loaded " + ChatColor.GRAY + "(" + config.getKeys(true).size() + " keys)");
+        getServer().getConsoleSender().sendMessage("");
+    }
+
     public static ThunderChat getInstance() { return instance; }
     public GlobalChatManager getGlobalChatManager() { return globalChatManager; }
     public FilterManager getFilterManager() { return filterManager; }
